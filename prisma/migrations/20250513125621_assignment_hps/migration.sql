77e457e6f9cc55545582a3a67f908e564d412eb0
Rens Salaladaga <staleexam19@gmail.com>
2025-05-13 22:59:59 +0800

    feat: create assignment feature

---
+++ prisma/migrations/20250513125621_assignment_hps/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `Assignment` ADD COLUMN `hps` DECIMAL(65, 30) NULL;
--- src/services/graphql/resolvers/assignment.resolver.ts
+++ src/services/graphql/resolvers/assignment.resolver.ts
@@ -1,10 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 import gql from "graphql-tag";
-
+import { DateTimeResolver } from "graphql-scalars";
+import Joi from "joi";
+import { GraphQLError } from "graphql";
 
 const prisma = new PrismaClient();
 
 export const assignmentTypeDefs = gql`
+    scalar DateTime
+
     type Assignment {
         id: Int!
         title: String!
@@ -12,27 +16,46 @@ export const assignmentTypeDefs = gql`
 
         teacherAssignedSubjectId: Int!
 
-        teacherAssignedSubject: TeacherAssignedSubject!,
+        dueDate: DateTime
+
+        hps: Float
+
+        createdAt: DateTime
+        updatedAt: DateTime
+
+        teacherAssignedSubject: TeacherAssignedSubject!
         assignmentAttachments: [AssignmentAttachment!]!
+
+
+        assignmentSubmissions: [AssignmentSubmission]
     }
 
     type Query {
-        assignments: [Assignment!]!,
+        assignments(teacherSubjectId: Int!): [Assignment!]!,
         assignment: Assignment!
     }
 
     type Mutation {
-        createAssignment(title: String!, instructions: String, teacherAssignedSubjectId: Int!): Assignment!
+        createAssignment(title: String!, instructions: String, teacherAssignedSubjectId: Int!, hps: Float, dueDate: DateTime ): Assignment!
     }
 `;
 
 export const assignemtResolvers = {
+    DateTime: DateTimeResolver,
     Assignment: {
         assignmentAttachments: (parent: any) => prisma.assignmentAttachment.findMany({ where: { assignmentId: parent.id } }),
-        teacherAssignedSubject: (parent: any) => prisma.teacherAssignedSubject.findUnique({ where: { id: parent.teacherAssignedSubjectId } })
+        teacherAssignedSubject: (parent: any) => prisma.teacherAssignedSubject.findUnique({ where: { id: parent.teacherAssignedSubjectId } }),
+        assignmentSubmissions: (parent: any) => prisma.assignmentSubmission.findMany({ where: { assignmentId: parent.id }, orderBy: { createdAt: "desc" } })
     },
     Query: {
-        assignments: () => prisma.assignment.findMany(),
+        assignments: (_: any, args: { teacherSubjectId: number; }) => prisma.assignment.findMany({
+            where: {
+                teacherAssignedSubjectId: args.teacherSubjectId
+            },
+            orderBy: {
+                createdAt: "desc"
+            }
+        }),
         assignment: (_: any, args: { id: number; }) => prisma.assignment.findUnique({ where: { id: args.id } })
     },
     Mutation: {
@@ -46,6 +69,28 @@ type Args = {
     teacherAssignedSubjectId: number;
 };
 
+const assignmentSchema = Joi.object({
+    title: Joi.string().required(),
+    instructions: Joi.string().optional(),
+    teacherAssignedSubjectId: Joi.number().integer().required(),
+    dueDate: Joi.date().iso().required(),
+    hps: Joi.number().precision(2).optional(),
+});
+
 async function createAssignment(_: any, args: Args) {
-    return await prisma.assignment.create({ data: args });
+    const { error, value } = assignmentSchema.validate(args);
+
+    if (error) {
+        throw new GraphQLError('Validation error', {
+            extensions: {
+                code: 'VALIDATION_ERROR',
+                errors: error.details.map((err: any) => ({
+                    path: err.path[0],  // e.g., 'title'
+                    message: err.message // e.g., '"title" is required'
+                })),
+            },
+        });
+    }
+
+    return await prisma.assignment.create({ data: value });
 }
